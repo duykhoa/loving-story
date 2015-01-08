665e1c58868c5d2acbd74a889a957521c8f0a107
Tranxuanvu <xuanvu442@gmail.com>
2015-01-08 13:43:31 +0700

    config rspec: remove test rspec email for service orders

--- spec/models/service_order_spec.rb
+++ spec/models/service_order_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe ServiceOrder do
-  it { should validate_presence_of(:email) }
   it { should validate_presence_of(:his_name) }
   it { should validate_presence_of(:her_name) }
   it { should validate_presence_of(:his_story) }
