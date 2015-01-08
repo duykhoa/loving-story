2b53eccc4457537e4e4f39dd8fdd23b01058cc50
Tranxuanvu <xuanvu442@gmail.com>
2015-01-08 13:53:16 +0700

    add column our_relationship and your_name to service orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107160103) do
+ActiveRecord::Schema.define(version: 20150108065133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 20150107160103) do
     t.string   "her_name"
     t.text     "his_story"
     t.text     "her_story"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "status",     default: 0
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.integer  "status",           default: 0
+    t.text     "our_relationship"
+    t.string   "your_name"
   end
 
 end
