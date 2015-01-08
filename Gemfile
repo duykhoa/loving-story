244a3e120464f6d2ba0a62496db9e7c479336bcf
Duy Khoa <duykhoa12t@gmail.com>
2015-01-08 11:23:30 +0700

    add shoulda matcher gem

--- Gemfile
+++ Gemfile
@@ -71,6 +71,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'rubocop'
   gem 'thin'
+  gem 'shoulda-matchers', require: false
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104085900) do
+ActiveRecord::Schema.define(version: 20150107160103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 20150104085900) do
     t.string   "her_name"
     t.text     "his_story"
     t.text     "her_story"
-    t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "status",     default: "pending"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
 end
