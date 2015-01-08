a5df5fb0f0b58a052a7a6970086dca5fc1b91912
Tranxuanvu <xuanvu442@gmail.com>
2015-01-08 13:40:01 +0700

    validate email

--- app/models/service_order.rb
+++ app/models/service_order.rb
@@ -1,6 +1,7 @@
 class ServiceOrder < ActiveRecord::Base
   enum status: [ :pending, :approve, :reject]
 
-  validates :email, :his_name, :her_name, :his_story, :her_story, presence: true
+  validates :his_name, :her_name, :his_story, :her_story, presence: true
   validates :status, inclusion: { in: statuses, message: 'status is invalid' }, allow_nil: true
+  validates :email, email: true
 end
