0ae1905f52aec7851ad55aae230c344272093224
Tranxuanvu <xuanvu442@gmail.com>
2015-01-10 17:10:37 +0700

    add field and config css header_image for service orders

--- app/models/service_order.rb
+++ app/models/service_order.rb
@@ -4,4 +4,8 @@ class ServiceOrder < ActiveRecord::Base
   validates :his_name, :her_name, :his_story, :her_story, :our_relationship, :your_name, presence: true
   validates :status, inclusion: { in: statuses, message: 'status is invalid' }, allow_nil: true
   validates :email, email: true
+
+  has_attached_file :header_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :header_image, :content_type => /\Aimage\/.*\Z/
+
 end
