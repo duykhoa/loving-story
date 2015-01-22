08ebe49b2b29c109594c0aa74000aa947adadfb9
Nguyễn Minh Trí <tendoumt1994@gmail.com>
2015-01-22 11:25:06 +0700

    remove some unused behavior in spec controller && routing

--- spec/routing/service_orders_routing_spec.rb
+++ spec/routing/service_orders_routing_spec.rb
@@ -3,10 +3,6 @@ require "rails_helper"
 RSpec.describe ServiceOrdersController, :type => :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/service_orders").to route_to("service_orders#index")
-    end
-
     it "routes to #new" do
       expect(:get => "/service_orders/new").to route_to("service_orders#new")
     end
@@ -15,20 +11,8 @@ RSpec.describe ServiceOrdersController, :type => :routing do
       expect(:get => "/service_orders/1").to route_to("service_orders#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/service_orders/1/edit").to route_to("service_orders#edit", :id => "1")
-    end
-
     it "routes to #create" do
       expect(:post => "/service_orders").to route_to("service_orders#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/service_orders/1").to route_to("service_orders#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/service_orders/1").to route_to("service_orders#destroy", :id => "1")
     end
 
   end
