77531fc7f58e769878488d2bc1c18d1fbc98443a
tranquangvu <vutran.513@gmail.com>
2015-01-23 14:24:28 +0700

    complete facebook like and comment

--- app/assets/javascripts/service_orders_show_page.js
+++ app/assets/javascripts/service_orders_show_page.js
@@ -25,3 +25,12 @@ $(".gallery").fancybox({
   openEffect: 'fade',
   closeEffect: 'fade'
 });
+
+// social plugin
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
