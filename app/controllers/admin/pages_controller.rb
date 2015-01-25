9f8392a6b07affe13f896ff45acaaee76d1be7e8
Nguyễn Minh Trí <tendoumt1994@gmail.com>
2015-01-25 17:06:17 +0700

    create, edit, delete otherpages for admin page

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,47 +1,42 @@
 class Admin::PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
-
   def index
-    @pages = Page.all
-    respond_with(@pages)
+    @dynamics = Page.all
   end
 
   def show
-    respond_with(@page)
   end
 
   def new
-    @page = Page.new
-    respond_with(@page)
+    @dynamic = Page.new
   end
 
   def edit
   end
 
   def create
-    @page = Page.new(page_params)
-    @page.save
-    respond_with(@page)
+    @dynamic = Page.new(page_params)
+    @dynamic.save
+    redirect_to admin_pages_path
   end
 
   def update
-    @page.update(page_params)
-    respond_with(@page)
+    @dynamic.update(page_params)
+    redirect_to admin_pages_path
   end
 
   def destroy
-    @page.destroy
-    respond_with(@page)
+    @dynamic.destroy
+    redirect_to admin_pages_path
   end
 
   private
     def set_page
-      @page = Page.friendly.find(params[:id])
+      @dynamic = Page.friendly.find(params[:id])
     end
 
     def page_params
-      params.require(:page).permit(:name, :content, :slug)
+      params.require(:page).permit(:name, :content)
     end
 end
