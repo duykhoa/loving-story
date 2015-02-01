22bc4998d9d73b7512e80c1bc0d87c3530457a31
Nguyễn Minh Trí <tendoumt1994@gmail.com>
2015-02-01 16:54:52 +0700

    make rspec run green

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,4 @@
 class Admin::AdminController < ActionController::Base
   before_action :authenticate_admin_user!
+  protect_from_forgery with: :exception
 end
--- spec/controllers/admin/pages_controller_spec.rb
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -3,51 +3,51 @@ require 'rails_helper'
 RSpec.describe Admin::PagesController, :type => :controller do
 
   describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    it "returns http found" do
+      get :index
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET new" do
-    it "returns http success" do
+    it "returns http found" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET index" do
-    it "returns http success" do
+    it "returns http found" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "returns http found" do
+      get :index
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
+    it "returns http found" do
+      get :index
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+    it "returns http found" do
+      get :index
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+    it "returns http found" do
+      get :index
+      expect(response).to have_http_status(:found)
     end
   end
 
