719f12fb81176e8aeb9f4e2e6bacd4429249f2a2
Tranxuanvu <xuanvu442@gmail.com>
2015-02-02 20:44:44 +0700

    WIP: create table template

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110083747) do
+ActiveRecord::Schema.define(version: 20150202133843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 20150110083747) do
     t.string   "header_image_content_type"
     t.integer  "header_image_file_size"
     t.datetime "header_image_updated_at"
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.string   "template_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
