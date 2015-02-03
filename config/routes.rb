eb8cdd2b39822f3a31bb2acce5a3546a43afaf49
Duy Khoa <duykhoa12t@gmail.com>
2015-02-03 22:41:52 +0700

    add errors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/show'
+
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   namespace :admin do
     resources :service_orders, only: [:index, :edit, :update, :destroy]
