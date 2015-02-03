7fe0e57dc3384509e84c41c998e5c2d2aa51c6c4
Duy Khoa <duykhoa12t@gmail.com>
2015-02-03 23:54:52 +0700

    add comment for enable gaffe in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,9 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local = false
+  # config.consider_all_requests_local = true
+  config.consider_all_requests_local = false # enable gaffe catching exeption
+
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
