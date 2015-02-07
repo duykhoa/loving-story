ced3d963505c2801208ba4aaff0306613c13e519
tranquangvu <vutran.513@gmail.com>
2015-02-07 10:53:41 +0700

    WIP: add rspec for setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,11 +1,24 @@
 class Setting < ActiveRecord::Base
+  SETTING_KEY = "all_text_settings"
+
   after_commit :flush_cache
 
-  def self.cached_settings
-    Rails.cache.fetch("all_text_settings") { all.to_a }
+  class << self
+    def cached_settings
+      fetch_cached
+    end
+
+    def map_data
+      all.
+      map { |setting| { setting.key => setting.value } }
+    end
+
+    def fetch_cached
+      Rails.cache.fetch(SETTING_KEY) { map_data }
+    end
   end
 
   def flush_cache
-    Rails.cache.delete("all_text_settings")
+    Rails.cache.delete(SETTING_KEY)
   end
 end
--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Setting, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Setting, :type => :model do
+  describe ".cached_settings" do
+    before do
+      allow(Rails).to receive_message_chain(:cache, :fetch).and_return([])
+      Setting.cached_settings
+    end
+
+    it "calls Rails.cache" do
+      expect(Rails).to have_received(:cache).once
+    end
+  end
+
+  describe "map_data" do
+    let!(:settings) { create_list(:setting, 10) }
+    let(:data) { Setting.map_data }
+
+    it "returns 10 setting items" do
+      expect(data.count).to eq(10)
+    end
+
+    it "return an array of 10 hash" do
+      expect(data.first).to be_a(Hash)
+    end
+  end
 end
