cf1e5cb1290b7aa065643e11bb734fa4631e337c
tranquangvu <vutran.513@gmail.com>
2015-02-08 19:45:43 +0700

    Fix rspec for setting model

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -1,30 +1,6 @@
 require 'rails_helper'
 
 describe Setting, :type => :model do
-  describe ".map_data" do
-    let!(:settings) { create_list(:setting, 10) }
-    let(:data) { Setting.map_data }
-
-    context "over look of setting data" do
-      it "returns 10 setting items" do
-        expect(data.keys.count).to eq(10)
-      end
-
-      it "return an array of 10 hash" do
-        expect(data).to be_a(Hash)
-      end
-    end
-
-    context "when get value of a setting" do
-      let!(:custom_setting) { create(:setting, key: "custom_key", value: "CUSTOM VALUE") }
-
-      it "returns value of custom_setting" do
-        value = data[:custom_key]
-        expect(value).to eq("CUSTOM VALUE")
-      end
-    end
-  end
-
   describe "value_of" do
     let(:setting_datas) { { key1: "value1", key2: "value2" } }
 
