45b7a6d4ad03648820557b4e97b6639cadf6ea41
Nguyễn Minh Trí <tendoumt1994@gmail.com>
2015-02-26 15:32:32 +0700

    adding public pages_controller and others dependency

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  before_action :set_page, only: [:show]
+
+  def show
+  end
+
+  private
+  def set_page
+    @page = Page.friendly.find(page_id[:id])
+  end
+
+  def page_id
+    params.permit(:id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   devise_for :admin_users
   get 'homes/index'
   resources :service_orders, only: [:new, :create, :show]
+  resources :pages, only: [:show]
 
   match '/' => 'service_orders#show', :constraints => { :subdomain => /.+.4ever/ }, via: [:get]
   root to: 'homes#index', subdomain: false
